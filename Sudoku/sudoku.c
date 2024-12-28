16934e7765e49867797bf66bef59a3d69d5bebe8
Muhammad Ali <proqualitythings100@gmail.com>
2024-12-28 17:16:28 +0500

    in progress

--- Sudoku/sudoku.c
+++ Sudoku/sudoku.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
+
+int validMove(int sudoku[9][9], int row, int col, int value);
+void printSudoku(int sudoku[9][9]);
+
 int sudoku[9][9] = {
     {3,0,0,0,2,0,0,7,0},
     {9,0,0,5,0,0,0,1,4},
@@ -14,7 +18,60 @@ int sudoku[9][9] = {
 
 int main() {
 
-
+    printf("\n\tWelcome To Sudoku Solver!\n");
+    printf("\nOriginal Puzzle.");
+    printSudoku(sudoku);
 
     return 0;
+}
+
+int validMove(int sudoku[9][9], int row, int col, int value) {
+    // valid row
+    for(int i = 0; i < 9; i++) {
+        if(sudoku[row][i] == value) {
+            return 0;
+        }
+    }
+
+    // valid column
+    for(int i = 0; i < 9; i++) {
+        if(sudoku[i][col] == value) {
+            return 0;
+        }
+    }
+
+    // valid square
+    int r = row - row % 3;
+    int c = col - col % 3;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            if(sudoku[r+i][c+j] == value) {
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
+
+void printSudoku(int sudoku[9][9]) {
+    printf("\n+-------+-------+-------+\n");
+    for(int row = 0; row < 9; row++) {
+        if(row % 3 == 0 && row != 0) {
+            printf("|-------+-------+-------|\n");
+        }
+        for(int col = 0; col < 9; col++) {
+            if(col % 3 == 0) {
+                printf("| "); 
+            }
+            if(sudoku[row][col] != 0) {
+                printf("%d ", sudoku[row][col]);
+            } else {
+                printf("  "); 
+            }
+        }
+        printf("|\n");
+    }
+    printf("+-------+-------+-------+");
 }
